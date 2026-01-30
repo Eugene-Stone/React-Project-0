90ce1a012cf36c049320e929efa4883deca5e885
Eugene-Stone <www.kraiser@gmail.com>
2026-01-30 18:05:14 +0200

    Update

--- src/TASKS/Lesson-16.jsx
+++ src/TASKS/Lesson-16.jsx
@@ -133,16 +133,105 @@ function createConnection2(serverUrl, roomId) {
 
 // ====================================================
 /* 
+Этот эффект зависит от объекта options , который создается заново каждый раз, когда вы редактируете ввод:
+
+Чтобы устранить эту проблему, переместите создание объекта options внутри эффекта, чтобы эффект зависел только от строки roomId :
+*/
+
+const serverUrl3 = 'https://localhost:12345';
+
+function ChatRoom3({ roomId }) {
+	const [message, setMessage] = useState('');
+
+	// const options = {
+	// 	serverUrl: serverUrl3,
+	// 	roomId: roomId,
+	// };
+
+	useEffect(() => {
+		const options = {
+			serverUrl: serverUrl3,
+			roomId: roomId,
+		};
+
+		const connection = createConnection3(options);
+		connection.connect();
+		return () => connection.disconnect();
+	}, [roomId]);
+	// }, [options]);
+
+	return (
+		<>
+			<h1>Welcome to the {roomId} room!</h1>
+			<input value={message} onChange={(e) => setMessage(e.target.value)} />
+		</>
+	);
+}
+
+function ChatRoomApp3() {
+	const [roomId, setRoomId] = useState('general');
+	return (
+		<>
+			<label>
+				Choose the chat room:{' '}
+				<select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+					<option value="general">general</option>
+					<option value="travel">travel</option>
+					<option value="music">music</option>
+				</select>
+			</label>
+			<br />
+			<ChatRoom3 roomId={roomId} />
+		</>
+	);
+}
+
+function createConnection3({ serverUrl, roomId }) {
+	// A real implementation would actually connect to the server
+	return {
+		connect() {
+			console.log('✅ Connecting to "' + roomId + '" room at ' + serverUrl + '...');
+		},
+		disconnect() {
+			console.log('❌ Disconnected from "' + roomId + '" room at ' + serverUrl);
+		},
+	};
+}
+
+// ====================================================
+/* 
 В этом примере пользовательский хук usePointerPosition отслеживает положение курсора, а пользовательский хук useDelayedValue возвращает значение, которое «отстает» от переданного вами значения на определенное количество миллисекунд. 
 */
-function usePointerPosition() {
+function usePointerPosition(ref) {
 	const [position, setPosition] = useState({ x: 0, y: 0 });
+
 	useEffect(() => {
 		function handleMove(e) {
-			setPosition({ x: e.clientX, y: e.clientY });
+			if (!ref.current) return;
+
+			const rect = ref.current.getBoundingClientRect();
+
+			/* 
+			rect содержит:
+
+			{
+				left,
+				top,
+				width,
+				height
+			}
+			*/
+
+			setPosition({
+				x: e.clientX - rect.left,
+				y: e.clientY - rect.top,
+			});
 		}
-		window.addEventListener('pointermove', handleMove);
-		return () => window.removeEventListener('pointermove', handleMove);
+
+		const el = ref.current;
+
+		el.addEventListener('pointermove', handleMove);
+		return () => el.removeEventListener('pointermove', handleMove);
 	}, []);
 	return position;
 }
@@ -160,13 +249,22 @@ function useDelayedValue(value, delay) {
 }
 
 function Canvas() {
-	const pos1 = usePointerPosition();
+	const containerRef = useRef(null);
+
+	const pos1 = usePointerPosition(containerRef);
 	const pos2 = useDelayedValue(pos1, 100);
 	const pos3 = useDelayedValue(pos2, 200);
 	const pos4 = useDelayedValue(pos3, 100);
 	const pos5 = useDelayedValue(pos4, 50);
 	return (
-		<div style={{ backgroundColor: '#eee' }}>
+		<div
+			ref={containerRef}
+			style={{
+				position: 'relative',
+				height: 300,
+				backgroundColor: '#eee',
+				overflow: 'hidden',
+			}}>
 			<Dot position={pos1} opacity={1} />
 			<Dot position={pos2} opacity={0.8} />
 			<Dot position={pos3} opacity={0.6} />
@@ -184,10 +282,10 @@ function Dot({ position, opacity }) {
 				backgroundColor: 'pink',
 				borderRadius: '50%',
 				opacity,
-				transform: `translate(${position.x}px, ${position.y}px)`,
 				pointerEvents: 'none',
-				left: -20,
-				top: -20,
+				transform: 'translate(-50%,-50%)',
+				left: position.x,
+				top: position.y,
 				width: 40,
 				height: 40,
 			}}
@@ -195,6 +293,8 @@ function Dot({ position, opacity }) {
 	);
 }
 
+// ====================================================
+// ====================================================
 // ====================================================
 // ====================================================
 // ====================================================
@@ -213,6 +313,8 @@ function mainFunc() {
 			<ChatRoom />
 			<hr />
 			<ChatApp />
+			<hr />
+			<ChatRoomApp3 />
 			<hr />
 			<Canvas />
 			<hr />
