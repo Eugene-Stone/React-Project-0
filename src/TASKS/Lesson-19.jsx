2e43146cef9dfb605e3ff6e412e5cbf8c7a4ca4d
Eugene-Stone <www.kraiser@gmail.com>
2026-02-10 11:50:58 +0200

    Update

--- src/TASKS/Lesson-19.jsx
+++ src/TASKS/Lesson-19.jsx
@@ -7,7 +7,7 @@
 	https://ru.react.dev/learn/removing-effect-dependencies
 	Removing Effect Dependencies
 */
-import { useState, useEffect, useEffectEvent } from 'react';
+import { useState, useEffect, useEffectEvent, useRef } from 'react';
 
 /* С помощью этого кода вы можете быть уверены, что всегда существует активное соединение с выбранным в данный момент сервером чата, независимо от конкретных взаимодействий, выполняемых пользователем. */
 export function sendMessage(message, roomId) {
@@ -422,7 +422,7 @@ function Timer3() {
 			setCountEvent();
 			/* Или так */
 			// setCount((c) => c + 1);
-		}, 1000);
+		}, 100000);
 		return () => {
 			console.log('❌ Clearing an interval');
 			clearInterval(id);
@@ -432,6 +432,372 @@ function Timer3() {
 	return <h1>Counter: {count}</h1>;
 }
 
+// ====================================================
+/* 
+В этом примере, когда вы нажимаете «Показать», появляется приветственное сообщение. Анимация длится секунду. При нажатии «Удалить» приветственное сообщение сразу исчезает. Логика плавного появления анимации реализована в файле animation.js в виде простого цикла анимации JavaScript. Вам не нужно менять эту логику. Вы можете относиться к ней как к сторонней библиотеке.Ваш эффект создает экземпляр FadeInAnimation для узла DOM, а затем вызывает start(duration) или stop() для управления анимацией.duration регулируется ползунком. Отрегулируйте ползунок и посмотрите, как изменится анимация.
+Этот код уже работает, но вы хотите кое-что изменить. В настоящее время при перемещении ползунка, управляющего переменной состояния duration , анимация перезапускается. Измените поведение так, чтобы Эффект не «реагировал» на переменную duration . Когда вы нажимаете «Показать», эффект должен использовать текущую duration на ползунке. Однако само по себе перемещение ползунка не должно перезапускать анимацию.
+*/
+export class FadeInAnimation {
+	constructor(node) {
+		this.node = node;
+	}
+	start(duration) {
+		this.duration = duration;
+		if (this.duration === 0) {
+			// Jump to end immediately
+			this.onProgress(1);
+		} else {
+			this.onProgress(0);
+			// Start animating
+			this.startTime = performance.now();
+			this.frameId = requestAnimationFrame(() => this.onFrame());
+		}
+	}
+	onFrame() {
+		const timePassed = performance.now() - this.startTime;
+		const progress = Math.min(timePassed / this.duration, 1);
+		this.onProgress(progress);
+		if (progress < 1) {
+			// We still have more frames to paint
+			this.frameId = requestAnimationFrame(() => this.onFrame());
+		}
+	}
+	onProgress(progress) {
+		this.node.style.opacity = progress;
+	}
+	stop() {
+		cancelAnimationFrame(this.frameId);
+		this.startTime = null;
+		this.frameId = null;
+		this.duration = 0;
+	}
+}
+
+function Welcome({ duration }) {
+	const ref = useRef(null);
+	const durationAnim = duration;
+
+	// Или так
+	const onAppear = useEffectEvent((animation) => {
+		animation.start(duration);
+	});
+	useEffect(() => {
+		const animation = new FadeInAnimation(ref.current);
+
+		// animation.start(duration);
+		animation.start(durationAnim);
+
+		// // Или так
+		// onAppear(animation);
+
+		return () => {
+			animation.stop();
+		};
+		// }, [duration]);
+	}, []);
+
+	return (
+		<h1
+			ref={ref}
+			style={{
+				opacity: 0,
+				color: 'white',
+				padding: 50,
+				textAlign: 'center',
+				fontSize: 50,
+				backgroundImage:
+					'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)',
+			}}>
+			Welcome
+		</h1>
+	);
+}
+
+function AppAnimation() {
+	const [duration, setDuration] = useState(1000);
+	const [show, setShow] = useState(false);
+
+	return (
+		<>
+			<label>
+				<input
+					type="range"
+					min="100"
+					max="10000"
+					value={duration}
+					onChange={(e) => setDuration(Number(e.target.value))}
+				/>
+				<br />
+				Fade in duration: {duration} ms
+			</label>
+			<button onClick={() => setShow(!show)}>{show ? 'Remove' : 'Show'}</button>
+			<br />
+			<div style={{ height: 160 }}>{show && <Welcome duration={duration} />}</div>
+		</>
+	);
+}
+
+// ====================================================
+/* 
+В этом примере каждый раз, когда вы нажимаете «Переключить тему», чат повторно подключается. Почему это происходит? Исправьте ошибку, чтобы чат повторно подключался только тогда, когда вы редактируете URL-адрес сервера или выбираете другую комнату чата.
+Относитесь к chat.js как к внешней сторонней библиотеке: вы можете обратиться к ней, чтобы проверить ее API, но не редактируйте ее.
+
+Если вы будете придерживаться примитивных свойств, где это возможно, это облегчит последующую оптимизацию ваших компонентов.
+*/
+export function createConnection3({ serverUrl, roomId }) {
+	// A real implementation would actually connect to the server
+	if (typeof serverUrl !== 'string') {
+		throw Error('Expected serverUrl to be a string. Received: ' + serverUrl);
+	}
+	if (typeof roomId !== 'string') {
+		throw Error('Expected roomId to be a string. Received: ' + roomId);
+	}
+	return {
+		connect() {
+			console.log('✅ Connecting to "' + roomId + '" room at ' + serverUrl + '...');
+		},
+		disconnect() {
+			console.log('❌ Disconnected from "' + roomId + '" room at ' + serverUrl);
+		},
+	};
+}
+
+function ChatRoom3({ options }) {
+	const { serverUrl, roomId } = options;
+
+	useEffect(() => {
+		// const connection = createConnection3(options);
+		const connection = createConnection3({
+			serverUrl: serverUrl,
+			roomId: roomId,
+		});
+		connection.connect();
+		return () => connection.disconnect();
+		// }, [options]);
+	}, [serverUrl, roomId]);
+
+	return <h1>Welcome to the {options.roomId} room!</h1>;
+}
+
+function App3() {
+	const [isDark, setIsDark] = useState(false);
+	const [roomId, setRoomId] = useState('general');
+	const [serverUrl, setServerUrl] = useState('https://localhost:1234');
+
+	const options = {
+		serverUrl: serverUrl,
+		roomId: roomId,
+	};
+
+	return (
+		<div className={isDark ? 'dark' : 'light'}>
+			<button onClick={() => setIsDark(!isDark)}>Toggle theme</button>
+			<label>
+				Server URL:{' '}
+				<input value={serverUrl} onChange={(e) => setServerUrl(e.target.value)} />
+			</label>
+			<label>
+				Choose the chat room:{' '}
+				<select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+					<option value="general">general</option>
+					<option value="travel">travel</option>
+					<option value="music">music</option>
+				</select>
+			</label>
+			<br />
+			<ChatRoom3 options={options} />
+		</div>
+	);
+}
+
+// ====================================================
+/* 
+В этом примере подключение к чату осуществляется либо с шифрованием, либо без него. Установите флажок и обратите внимание на разные сообщения в консоли, когда шифрование включено и выключено. Попробуйте сменить комнату. Затем попробуйте переключить тему. Когда вы подключены к чату, вы будете получать новые сообщения каждые несколько секунд. Убедитесь, что их цвет соответствует выбранной вами теме.
+
+В этом примере чат повторно подключается каждый раз, когда вы пытаетесь сменить тему. Исправьте это. После исправления изменение темы не должно повторно подключать чат, но переключение настроек шифрования или изменение комнаты должно повторно подключиться.
+
+Не меняйте код в chat.js Помимо этого, вы можете изменить любой код, если это приведет к тому же поведению. Например, вам может оказаться полезным изменить передаваемые реквизиты.
+*/
+
+export function showNotification(message, theme) {
+	const themeNotification = theme === 'dark' ? ' - Black' : ' - White';
+	console.log(message + themeNotification + ' theme');
+}
+export function createEncryptedConnection({ serverUrl, roomId }) {
+	// A real implementation would actually connect to the server
+	if (typeof serverUrl !== 'string') {
+		throw Error('Expected serverUrl to be a string. Received: ' + serverUrl);
+	}
+	if (typeof roomId !== 'string') {
+		throw Error('Expected roomId to be a string. Received: ' + roomId);
+	}
+	let intervalId;
+	let messageCallback;
+	return {
+		connect() {
+			console.log('✅ 🔐 Connecting to "' + roomId + '" room... (encrypted)');
+			clearInterval(intervalId);
+			intervalId = setInterval(() => {
+				if (messageCallback) {
+					if (Math.random() > 0.5) {
+						messageCallback('hey');
+					} else {
+						messageCallback('lol');
+					}
+				}
+			}, 3000);
+		},
+		disconnect() {
+			clearInterval(intervalId);
+			messageCallback = null;
+			console.log('❌ 🔐 Disconnected from "' + roomId + '" room (encrypted)');
+		},
+		on(event, callback) {
+			if (messageCallback) {
+				throw Error('Cannot add the handler twice.');
+			}
+			if (event !== 'message') {
+				throw Error('Only "message" event is supported.');
+			}
+			messageCallback = callback;
+		},
+	};
+}
+
+export function createUnencryptedConnection({ serverUrl, roomId }) {
+	// A real implementation would actually connect to the server
+	if (typeof serverUrl !== 'string') {
+		throw Error('Expected serverUrl to be a string. Received: ' + serverUrl);
+	}
+	if (typeof roomId !== 'string') {
+		throw Error('Expected roomId to be a string. Received: ' + roomId);
+	}
+	let intervalId;
+	let messageCallback;
+	return {
+		connect() {
+			console.log('✅ Connecting to "' + roomId + '" room (unencrypted)...');
+			clearInterval(intervalId);
+			intervalId = setInterval(() => {
+				if (messageCallback) {
+					if (Math.random() > 0.5) {
+						messageCallback('hey');
+					} else {
+						messageCallback('lol');
+					}
+				}
+			}, 3000);
+		},
+		disconnect() {
+			clearInterval(intervalId);
+			messageCallback = null;
+			console.log('❌ Disconnected from "' + roomId + '" room (unencrypted)');
+		},
+		on(event, callback) {
+			if (messageCallback) {
+				throw Error('Cannot add the handler twice.');
+			}
+			if (event !== 'message') {
+				throw Error('Only "message" event is supported.');
+			}
+			messageCallback = callback;
+		},
+	};
+}
+
+function ChatRoom4({ roomId, isEncrypted, onMessage }) {
+	//
+	const onNotificationMessage = useEffectEvent((connection) => {
+		connection.on('message', (msg) => onMessage(msg));
+	});
+
+	// Или так из учебника
+	// const onReceiveMessage = useEffectEvent(onMessage);
+
+	useEffect(() => {
+		function createConnection4() {
+			const options = {
+				serverUrl: 'https://localhost:1234',
+				roomId: roomId,
+			};
+			if (isEncrypted) {
+				return createEncryptedConnection(options);
+			} else {
+				return createUnencryptedConnection(options);
+			}
+		}
+
+		const connection = createConnection4();
+		// connection.on('message', (msg) => onMessage(msg));
+		onNotificationMessage(connection);
+
+		// Из учебника
+		// connection.on('message', (msg) => onReceiveMessage(msg));
+
+		connection.connect();
+		return () => connection.disconnect();
+		// }, [createConnection4, onMessage]);
+	}, [roomId, isEncrypted]);
+
+	return <h1>Welcome to the {roomId} room!</h1>;
+}
+
+function App4() {
+	const [isDark, setIsDark] = useState(false);
+	const [roomId, setRoomId] = useState('general');
+	const [isEncrypted, setIsEncrypted] = useState(false);
+
+	return (
+		<>
+			<label>
+				<input
+					type="checkbox"
+					checked={isDark}
+					onChange={(e) => setIsDark(e.target.checked)}
+				/>
+				Use dark theme
+			</label>
+			<label>
+				<input
+					type="checkbox"
+					checked={isEncrypted}
+					onChange={(e) => setIsEncrypted(e.target.checked)}
+				/>
+				Enable encryption
+			</label>
+			<label>
+				Choose the chat room:{' '}
+				<select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+					<option value="general">general</option>
+					<option value="travel">travel</option>
+					<option value="music">music</option>
+				</select>
+			</label>
+			<br />
+			<ChatRoom4
+				roomId={roomId}
+				// serverUrl={'https://localhost:1234'}
+				isEncrypted={isEncrypted}
+				onMessage={(msg) => {
+					showNotification('New message: ' + msg, isDark ? 'dark' : 'light');
+				}}
+				// createConnection4={() => {
+				// 	const options = {
+				// 		serverUrl: 'https://localhost:1234',
+				// 		roomId: roomId,
+				// 	};
+				// 	if (isEncrypted) {
+				// 		return createEncryptedConnection(options);
+				// 	} else {
+				// 		return createUnencryptedConnection(options);
+				// 	}
+				// }}
+			/>
+		</>
+	);
+}
+
+// ====================================================
 // ====================================================
 // ====================================================
 // ====================================================
@@ -451,6 +817,12 @@ function mainFunc() {
 			<App2 />
 			<hr />
 			<Timer3 />
+			<hr />
+			<AppAnimation />
+			<hr />
+			<App3 />
+			<hr />
+			<App4 />
 			<hr />
 		</>
 	);
